dadfcb3e1fda4c34a7e342f80dbcfa917b1fbf96
axel <axel.escoto@iteso.mx>
2025-04-11 19:34:22 -0600

    bulk commit :p

--- include/mfrc522.h
+++ include/mfrc522.h
@@ -77,6 +77,45 @@
 #define TestDAC2Reg           (0x3A << 1)    // defines the test value for TestDAC2
 #define TestADCReg            (0x3B << 1)    // shows the value of ADC I and Q channels
 
+// MFRC522 Commands. See the chapter 10 of the datasheet.
+#define Idle                    0x00
+#define Mem                     0x01
+#define GenerateRandomID        0x02
+#define CalcCRC                 0x03
+#define Transmit                0x04
+#define NoCmdChange             0x07
+#define Receive                 0x08
+#define Transceive              0x0C
+#define MFAuthent               0x0E
+#define SoftReset               0x0F
+
+// Commands sent to the PICC.
+// The commands used by the PCD to manage communication with several PICCs (ISO 14443-3, Type A, section 6.4)
+#define PICC_CMD_REQA           0x26    // REQuest command, Type A. Invites PICCs in state IDLE to go to READY and prepare for anticollision or selection. 7 bit frame.
+#define PICC_CMD_WUPA           0x52    // Wake-UP command, Type A. Invites PICCs in state IDLE and HALT to go to READY(*) and prepare for anticollision or selection. 7 bit frame.
+#define PICC_CMD_CT             0x88    // Cascade Tag. Not really a command, but used during anti collision.
+#define PICC_CMD_SEL_CL1        0x93    // Anti collision/Select, Cascade Level 1
+#define PICC_CMD_SEL_CL2        0x95    // Anti collision/Select, Cascade Level 2
+#define PICC_CMD_SEL_CL3        0x97    // Anti collision/Select, Cascade Level 3
+#define PICC_CMD_HLTA           0x50    // HaLT command, Type A. Instructs an ACTIVE PICC to go to state HALT.
+#define PICC_CMD_RATS           0xE0    // Request command for Answer To Reset.
+
+// The commands used for MIFARE Classic (from http://www.mouser.com/ds/2/302/MF1S503x-89574.pdf, Section 9)
+// Use PCD_MFAuthent to authenticate access to a sector, then use these commands to read/write/modify the blocks on the sector.
+// The read/write commands can also be used for MIFARE Ultralight.
+#define PICC_CMD_MF_AUTH_KEY_A  0x60    // Perform authentication with Key A
+#define PICC_CMD_MF_AUTH_KEY_B  0x61    // Perform authentication with Key B
+#define PICC_CMD_MF_READ        0x30    // Reads one 16 byte block from the authenticated sector of the PICC. Also used for MIFARE Ultralight.
+#define PICC_CMD_MF_WRITE       0xA0    // Writes one 16 byte block to the authenticated sector of the PICC. Called "COMPATIBILITY WRITE" for MIFARE Ultralight.
+#define PICC_CMD_MF_DECREMENT   0xC0    // Decrements the contents of a block and stores the result in the internal data register.
+#define PICC_CMD_MF_INCREMENT   0xC1    // Increments the contents of a block and stores the result in the internal data register.
+#define PICC_CMD_MF_RESTORE     0xC2    // Reads the contents of a block into the internal data register.
+#define PICC_CMD_MF_TRANSFER    0xB0    // Writes the contents of the internal data register to a block.
+
+// The commands used for MIFARE Ultralight (from http://www.nxp.com/documents/data_sheet/MF0ICU1.pdf, Section 8.6)
+// The PICC_CMD_MF_READ and PICC_CMD_MF_WRITE can also be used for MIFARE Ultralight.
+#define PICC_CMD_UL_WRITE       0xA2    // Writes one 4 byte page to the PICC.
+
 // Use logicar OR to combine the register address with the read bit
 #define MFRC522_READ_MSB        0x80          // MSB for read operation
 // Use logical AND to combine the register address with the write bit
@@ -91,6 +130,19 @@
 #define SPI_SPEED 500000                    // 1MHz
 #define SPI_BITS_PER_WORD 8                 // 8 bits per word
 #define SPI_MODE SPI_MODE_0                 // SPI mode 0 (CPOL=0, CPHA=0)
+
+// Status codes for MFRC522 operations
+typedef enum {
+    STATUS_OK,                // Success
+    STATUS_ERROR,             // Error in communication
+    STATUS_COLLISION,         // Collision detected
+    STATUS_TIMEOUT,           // Timeout in communication
+    STATUS_NO_ROOM,          // A buffer is not big enough
+    STATUS_INTERNAL_ERROR,    // Internal error in the code. Should not happen ;-)
+    STATUS_INVALID,           // Invalid argument
+    STATUS_CRC_WRONG,        // The CRC_A does not match
+    STATUS_MIFARE_NACK = 0xff // A MIFARE PICC responded with NAK
+} StatusCode;
 
 typedef struct {
     int spi_fd;
--- src/mfrc522.c
+++ src/mfrc522.c
@@ -1,10 +1,24 @@
 /**
  * MFRC522 Library
- * 
+ *
  * MFRC522 RFID reader connected to a PICO-PI-IMX8MM via SPI.
  */
 
 #include "../include/mfrc522.h"
+
+/**
+ * Initialize the MFRC522 chip
+ *  Init GPIO
+ *  Init SPI
+ *  Do reset
+ *  Reset baud rates
+ *  Reset ModWidthReg
+ *  Set timeout config
+ */
+int mfrc522_init(mfrc522 *dev)
+{
+    // TODO
+}
 
 /**
  * Initialize GPIO for MFRC522
@@ -104,7 +118,8 @@ int mfrc522_init_spi(mfrc522 *dev)
  *  See 8.1.2.1 in the MFRC522 datasheet for details.
  *  - The MSB of the register address is set to 1 to indicate a read operation (8.1.2.3).
  */
-uint8_t mfrc522_read_register(mfrc522 *dev, uint8_t reg) {
+uint8_t mfrc522_read_register(mfrc522 *dev, uint8_t reg)
+{
     uint8_t tx[2] = {MFRC522_READ_MSB | reg, 0};
     uint8_t rx[2] = {0, 0};
 
@@ -118,7 +133,8 @@ uint8_t mfrc522_read_register(mfrc522 *dev, uint8_t reg) {
     };
 
     int ret = ioctl(dev->spi_fd, SPI_IOC_MESSAGE(1), &tr);
-    if (ret < 0) {
+    if (ret < 0)
+    {
         perror("Error in SPI transfer");
         exit(EXIT_FAILURE);
     }
@@ -130,26 +146,144 @@ uint8_t mfrc522_read_register(mfrc522 *dev, uint8_t reg) {
 /**
  * Write a byte to the specified register
  */
-void mfrc522_write_register(mfrc522 *dev, uint8_t reg, uint8_t value) {
+void mfrc522_write_register(mfrc522 *dev, uint8_t reg, uint8_t value)
+{
     uint8_t tx[2] = {MFRC522_WRITE_MSB & reg, value};
     uint8_t rx[2] = {0, 0};
 
     struct spi_ioc_transfer tr = {
-        .tx_buf = (unsigned long) tx,
-        .rx_buf = (unsigned long) rx,
+        .tx_buf = (unsigned long)tx,
+        .rx_buf = (unsigned long)rx,
         .len = 2,
         .speed_hz = SPI_SPEED,
         .bits_per_word = SPI_BITS_PER_WORD,
-        .delay_usecs = 0
-    };
+        .delay_usecs = 0};
 
-    if (ioctl(dev->spi_fd, SPI_IOC_MESSAGE(1), &tr) < 0) {
+    if (ioctl(dev->spi_fd, SPI_IOC_MESSAGE(1), &tr) < 0)
+    {
         perror("Error in SPI transfer");
         exit(EXIT_FAILURE);
     }
 }
 
-bool mfrc522_isNewCardPresent(mfrc522 *dev) {
+/**
+ * Clear bits in the specified register
+ *  - The bits to clear are specified by the mask.
+ */
+void mfrc522_clearRegisterBitMask(mfrc522 *dev, uint8_t reg, uint8_t mask) {
+    uint8_t tmp = mfrc522_read_register(dev, reg);
+
+    // Clear bits in the register
+    tmp &= (~mask);
+    mfrc522_write_register(dev, reg, tmp);
+}
+
+/**
+ * Set bits in the specified register
+ */
+void mfrc522_setRegisterBitMask(mfrc522 *dev, uint8_t reg, uint8_t mask) {
+    uint8_t tmp = mfrc522_read_register(dev, reg);
+
+    mfrc522_write_register(dev, reg, tmp | mask);
+}
+
+uint8_t mfrc522_communicateWithPICC( //TODO: Add descriptions to arguments
+    mfrc522 *dev,
+    uint8_t command,
+    uint8_t waitIrq,
+    uint8_t *sendData,
+    uint8_t sendLen,
+    uint8_t *backData,
+    uint8_t *backLen,
+    uint8_t *validBits,
+    uint8_t rxAlign,
+    bool checkCRC
+) {
+    uint8_t txLastBits = validBits ? *validBits : 0;
+    uint8_t bitFraming = (rxAlign << 4) + txLastBits;       // RxAlign = BitFramingReg[6..4]. TxLastBits = BitFramingReg[2..0]
+
+    mfrc522_write_register(CommandReg, Idle);               // Stop any active command.
+    mfrc522_write_register(ComIrqReg, 0x7F);                // Clear all seven interrupt request bits.
+    mfrc522_write_register(FIFOLevelReg, 0x80);             // FlushBuffer = 1, FIFO init.
+    mfrc522_write_register(FIFODataReg, sendLen, sendData); // TODO: Implement multy byte write to register
+    mfrc522_write_register(BitFramingReg, bitFraming);      // Bit adjustments.
+    mfrc522_write_register(CommandReg, command);            // Execute command.
+
+    if (command == Transceive) {
+        mfrc522_setRegisterBitMask(BitFramingReg, 0x80);    // StartSend=1, transmission of data starts
+    }
+
+    // Wait for the command to complete (max 36ms)
+    uint32_t start_time = time(NULL) * 1000;  // Current time in milliseconds
+    uint32_t deadline = start_time + 36;
+    bool completed = false;
+
+    do {
+        uint8_t n = mfrc522_read_register(dev, ComIrqReg);  // ComIrqReg[7..0] bits are: Set1 TxIRq RxIRq IdleIRq HiAlertIRq LoAlertIRq ErrIRq TimerIRq
+        if (n & waitIrq) {                    // One of the interrupts that signal success has been set.
+            completed = true;
+            break;
+        }
+        if (n & 0x01) {                       // Timer interrupt - nothing received in 25ms
+            return STATUS_TIMEOUT;
+        }
+        usleep(1000);  // Sleep for 1ms instead of yield()
+    } while ((time(NULL) * 1000) < deadline);
+    
+    if (!completed) {
+        return STATUS_TIMEOUT;
+    }
+
+    // Stop if errors
+    uint8_t errorRegVal = mfrc522_read_register(dev, ErrorReg); // ErrorReg[7..0] bits are: WrErr TempErr reserved BufferOvfl CollErr CRCErr ParityErr ProtocolErr
+    if (errorRegVal & 0x13) {               // Check BufferOvfl ParityErr ProtocolErr
+        return STATUS_ERROR;
+    }
+
+    // Valid bits
+    uint8_t _valBits = 0;
+
+    // If the caller wants data back, get it from the MFRC522.
+    if (backData && backLen) {
+        uint8_t n = mfrc522_read_register(dev, FIFOLevelReg);       // Number of bytes in the FIFO
+        if (n > *backLen) {
+            return STATUS_NO_ROOM;
+        }
+        *backLen = n;
+        mfrc522_read_register(dev, n, backData, rxAlign);           // TODO: mult byte read
+        _valBits = mfrc522_read_register(dev, ControlReg) & 0x07;   // RxLastBits[2:0] indicates the number of valid bits in the last received byte. If this value is 000b, the whole byte is valid.
+        if (validBits) {
+            *validBits = _valBits;
+        }
+    }
+
+    // Tell collisions
+    if (errorRegVal & 0x08) {
+        return STATUS_COLLISION;
+    }
+
+    // Perform CRC_A validation if requested.
+
+    return STATUS_OK;
+}
+
+uint8_t mfrc522_transceive_data(
+        mfrc522 *dev,           
+        uint8_t *sendData,      // Pointer to the data to transfer to the FIFO.
+        uint8_t sendLen,        // Number of bytes to transfer to the FIFO.
+        uint8_t *backData,      // null ptr or pointer to the buffer if data should be read back after executing the command.
+        uint8_t *backLen,       // In: Max number of bytes to write to *backData. Out: The number of bytes returned.
+        uint8_t *validBits,     // In/Out: Number of valid bits in the last byte. 0 for 8 valid bits. Default nullptr.
+        uint8_t rxAlign,        // In: Defines the bit position in backData[0] for the first bit received. Default 0.
+        bool    checkCRC        // In: True => The last two bytes of the response is assumed to be a CRC_A that must be validated.
+    )
+{
+    uint8_t waitIRq = 0x30;
+    return mfrc522_communicateWithPICC();
+}
+
+bool mfrc522_isNewCardPresent(mfrc522 *dev)
+{
     uint8_t buff[2];
     uint8_t size = sizeof(buff);
 
@@ -160,9 +294,43 @@ bool mfrc522_isNewCardPresent(mfrc522 *dev) {
     // Reset ModWidthreg
     mfrc522_write_register(ModWidthReg, 0x26);
 
-    // Implement PICC_RequestA from mfrc522 miguelbalboa
-    
+    // Send request
+    uint8_t res = PICC_RequestA()
+}
 
+uint8_t PICC_RequestA(mfrc522 *dev, uint8_t *bufferATQA, uint8_t *bufferSize) {
+    return PICC_REQA_or_WUPA(dev, PICC_CMD_REQA, bufferATQA, bufferSize);
+}
+
+uint8_t PICC_REQA_or_WUPA(mfrc522 *dev, uint8_t command, uint8_t *bufferATQA, uint8_t *bufferSize)
+{
+    uint8_t validBits;
+    int status;
+
+    if (bufferATQA == NULL || *bufferSize < 2)
+    {
+        return STATUS_NO_ROOM;
+    }
+
+    // Clear collision register
+    mfrc522_clearRegisterBitMask(dev, CollReg, 0x80);
+
+    // Set valid bits for REQA and WUPA
+    validBits = 7;
+
+    // Transceive data
+    status = mfrc522_transceive_data(dev, &command, 1, bufferATQA, bufferSize, &validBits);
+    if (status != STATUS_OK)
+    {
+        return status;
+    }
+
+    if (*bufferSize != 2 || validBits != 0)
+    {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
 }
 
 /**
